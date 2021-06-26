03884112ec54c4422ebef2b7dc873d2529fdee6f
Patricio Fuentealba <patricio.fuentealba.feliu@gmail.com>
2021-06-25 20:12:16 -0400

    Json se envia correctamente

--- cartas.py
+++ cartas.py
@@ -3,26 +3,43 @@ class Card:
         self.nombre=nombre
         self.costo=coste
 
+    def CardToJson(self):
+        if isinstance(self, Unidad) == True :
+            json="{'nombre':"+self.nombre +",'costo':"+str(self.costo)+"}"
+            print("Unidad")
+
+        else:
+            json="{'nombre':'"+self.nombre +"','costo':"+str(self.costo)+" ,'stat':'"+self.stat+"' }"
+            print("Hechizo")
+            return json
+
 class Hechizo(Card):
     def __init__(self,nombre,coste,stat,magnitud):
-        super(nombre,coste)
+        super().__init__(nombre,coste)
         self.stat=stat
         self.magnitud=magnitud
     
     def lanzar(self,Objetivo):
         #identificar si es una unidad
-        if self.stat=="attack" :
-            Objetivo.attack+= self.magnitud
-        if self.stat=="defense" :
-            Objetivo.defense+= self.magnitud
-            
+        # if target instance Unit:
+        if isinstance(Objetivo, Unidad) == True :
+            if self.stat=="attack" :
+                Objetivo.attack+= self.magnitud
+            if self.stat=="defense" :
+                Objetivo.defense+= self.magnitud
+        else:
+            print( "Target must be a unit!")    
+
+    
 
 class Unidad(Card):
-    def __init__(self):
-        self.atttack=0
-        self.defense=0
+    def __init__(self,nombre,coste,ataque,defensa):
+        super().__init__(nombre,coste)
+        self.attack=ataque
+        self.defense=defensa
 
     def atacar(self,Objetivo):
         pass
+
 
 
