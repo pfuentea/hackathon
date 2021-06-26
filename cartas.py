f9589437a953987fa8bdb7ad878f9ba6cf456887
OctavioOp <oc.opazoutlook.com>
2021-06-25 21:59:30 -0400

    Se agrega diseño de carta unidad, problema con css

--- cartas.py
+++ cartas.py
@@ -1,28 +1,26 @@
 class Card:
-    def __init__(self,nombre,coste):
-        self.nombre=nombre
-        self.costo=coste
+    def __init__(self, nombre, coste):
+        self.nombre = nombre
+        self.costo = coste
+
 
 class Hechizo(Card):
-    def __init__(self,nombre,coste,stat,magnitud):
-        super(nombre,coste)
-        self.stat=stat
-        self.magnitud=magnitud
-    
-    def lanzar(self,Objetivo):
-        #identificar si es una unidad
-        if self.stat=="attack" :
-            Objetivo.attack+= self.magnitud
-        if self.stat=="defense" :
-            Objetivo.defense+= self.magnitud
-            
+    def __init__(self, nombre, coste, stat, magnitud):
+        super(nombre, coste)
+        self.stat = stat
+        self.magnitud = magnitud
+
+    def lanzar(self, Objetivo):
+        # identificar si es una unidad
+        if isinstance(Objetivo,Unidad)== True:
+            if self.stat == "attack":
+                Objetivo.attack += self.magnitud
+            if self.stat == "defense":
+                Objetivo.defense += self.magnitud
+
 
 class Unidad(Card):
     def __init__(self):
-        self.atttack=0
-        self.defense=0
-
-    def atacar(self,Objetivo):
-        pass
-
+        self.atttack = 0
+        self.defense = 0
 
