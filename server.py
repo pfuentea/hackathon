dae54ecb2608cc2fc514aaf4f69d8bae03b180b0
Patricio Fuentealba <patricio.fuentealba.feliu@gmail.com>
2021-06-26 11:05:22 -0400

    Se modifica el server para que se muestren de a una

--- server.py
+++ server.py
@@ -2,13 +2,14 @@ from flask import Flask, render_template
 from baraja import *
 app = Flask(__name__)
 
-
-
-objetos =baraja
-
+objetos=[]
+app.cont=0
+cartas_por_mostrar=[]
 
 @app.route("/")
 def hello_world():
+    objetos.append(baraja[app.cont])
+    app.cont+=1
     return render_template('index.html', objetos=objetos)
 
 
